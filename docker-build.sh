4614dae962ab8b2da767c66df69305d75813b4ed
ieee0824 <ieee0824@ezweb.ne.jp>
2016-10-19 16:14:28 +0900

    add build.Dockerfile

--- docker-build.sh
+++ docker-build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-export GOOS=linux
-export GOARCH=amd64
-export CGO_ENABLED=0
-
 mkdir -p build
 
-go build -o build/h2s github.com/goshinobi/h2sproxy/cmd/h2s
-
+docker build -t goshinobi/h2s-build -f build.Dockerfile .
+docker run -v `pwd`/build:/build goshinobi/h2s-build
 docker build -t goshinobi/h2s -f Dockerfile .
 
 rm -rf build
